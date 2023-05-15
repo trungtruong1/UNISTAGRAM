a278eec1a7995d86bc649d56d617bc1b1882f447
kurone <fnt.minhduc@gmail.com>
2023-05-15 21:27:26 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ git checkout milestone2
 mongod --fork --logpath /var/log/mongodb.log
 mongosh admin --eval "db.createUser({ user: 'admin', pwd: 'password', roles: ['userAdminAnyDatabase'] })"
 
-mongoimport --db=cse364 --collection=user --authenticationDatabase admin --username admin --password password --file data/users.json  --jsonArray
-mongoimport --db=cse364 --collection=conversation --authenticationDatabase admin --username admin --password password --file data/conversations.json  --jsonArray
-mongoimport --db=cse364 --collection=meme --authenticationDatabase admin --username admin --password password --file data/memes.json  --jsonArray
-mongoimport --db=cse364 --collection=message --authenticationDatabase admin --username admin --password password --file data/messages.json  --jsonArray
-mongoimport --db=cse364 --collection=reaction --authenticationDatabase admin --username admin --password password --file data/reactions.json  --jsonArray
+mongoimport --db=cse364 --collection=user --authenticationDatabase admin --username admin --password password --file data/users.json --jsonArray
+mongoimport --db=cse364 --collection=conversation --authenticationDatabase admin --username admin --password password --file data/conversations.json --jsonArray
+mongoimport --db=cse364 --collection=meme --authenticationDatabase admin --username admin --password password --file data/memes.json --jsonArray
+mongoimport --db=cse364 --collection=message --authenticationDatabase admin --username admin --password password --file data/messages.json --jsonArray
+mongoimport --db=cse364 --collection=reaction --authenticationDatabase admin --username admin --password password --file data/reactions.json --jsonArray
 
 mvn jacoco:report
 mvn package
