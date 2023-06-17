c432ba3fc9005e39b075cfcfe0e3954d74d8e828
kurone <fnt.minhduc@gmail.com>
2023-06-18 07:42:52 +0900

    modify: run.sh can now build .war file

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 git clone https://github.com/trungtruong1/UNISTAGRAM.git 
 cd UNISTAGRAM
-git checkout milestone2
+git checkout milestone3
+
 mongod --fork --logpath /var/log/mongodb.log
 mongosh admin --eval "db.createUser({ user: 'admin', pwd: 'password', roles: ['userAdminAnyDatabase'] })"
 
-mongoimport --db=cse364 --collection=user --authenticationDatabase admin --username admin --password password --file data/users.json --jsonArray
-mongoimport --db=cse364 --collection=conversation --authenticationDatabase admin --username admin --password password --file data/conversations.json --jsonArray
-mongoimport --db=cse364 --collection=meme --authenticationDatabase admin --username admin --password password --file data/memes.json --jsonArray
-mongoimport --db=cse364 --collection=message --authenticationDatabase admin --username admin --password password --file data/messages.json --jsonArray
-mongoimport --db=cse364 --collection=reaction --authenticationDatabase admin --username admin --password password --file data/reactions.json --jsonArray
+cd src/main/frontend/
+npm i
+npm run build
 
-mvn jacoco:report
-mvn package
-java -jar ./target/cse364-project-1.0-SNAPSHOT.jar
+cp -r ./build/. ../resources/static/
+
+cd ../../../
+mvn clean package
